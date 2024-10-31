de589540b26bd3c9350a482f8ab8bcfe17ddceef
JiWoongE <147473223+JiWoongE@users.noreply.github.com>
2024-10-31 22:49:11 +0900

    상태 적용 및 이벤트리스너 추가

---
+++ react-crash-course/src/components/PostsList.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+import NewPost from "./NewPost";
+import Post from "./Post";
+import classes from "./PostsList.module.css";
+
+function PostsList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function AuthorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  return (
+    <>
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={AuthorChangeHandler}
+      />
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Manuel" body="Check out the full course!" />
+      </ul>
+    </>
+  );
+}
+
+export default PostsList;
